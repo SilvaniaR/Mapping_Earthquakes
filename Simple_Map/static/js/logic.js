609bae989c732e07d01e12474ada5c0370a7ba15
SilvaniaR <Silvania.roche@outlook.com>
2022-03-17 11:48:39 -0400

    adding file changes.

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -23,11 +23,3 @@ streets.addTo(map);
 
 // We create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
-//  Add a marker to the map for Los Angeles, California.
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-L.circle([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: '#ffffa1'
-}).addTo(map);
